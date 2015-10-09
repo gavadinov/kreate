94304a54ad66d7f6d7e7eb310c2eec0b6bc271a9
Lyubomir Gavadinov <gavadinov@gmail.com>
2015-10-09 17:43:27 +0300

    Update AbstractService.php

--- app/Framework/Service/AbstractService.php
+++ app/Framework/Service/AbstractService.php
@@ -1,11 +1,30 @@
 <?php
 namespace Framework\Service;
 
+use Framework\Factory\RepositoryFactory;
 abstract class AbstractService
 {
 	protected $singleton = false;
 
-	public function getIsSingleton()
+	/**
+	 *
+	 * @return \Framework\Persistence\AbstractMySqlRepository
+	 */
+	protected function createRepo()
+	{
+		return RepositoryFactory::create($this->getName());
+	}
+
+	public function __construct(array $params = array())
+	{}
+
+	public function getName()
+	{
+		$nameArr = explode('\\', get_class($this));
+		return preg_replace('/Service/', '', end($nameArr));
+	}
+
+	public function isSingleton()
 	{
 		return $this->singleton;
 	}
